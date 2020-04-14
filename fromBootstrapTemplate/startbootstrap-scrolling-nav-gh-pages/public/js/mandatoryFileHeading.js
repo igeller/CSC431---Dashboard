84ad954c65f45849cc306f3fe59ab1b8ae6ac38a
IG <ieg23@miami.edu>
2020-04-13 20:23:42 -0400

    fixed and reduce
    fixed the login modal popups not working
    
    moved the nav stuff into the mandatory file (still needs some work)

--- fromBootstrapTemplate/startbootstrap-scrolling-nav-gh-pages/public/js/mandatoryFileHeading.js
+++ fromBootstrapTemplate/startbootstrap-scrolling-nav-gh-pages/public/js/mandatoryFileHeading.js
@@ -27,17 +27,48 @@ $(document).ready(function(){
 
     addTag("head", 'link', ["rel", "type", "href"], ["stylesheet", "text/css", "./css/mandatoryStylings.css"])
 
-    addTag("head", "link", ["src"], ["https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"]);
-    addTag("head", "link", ["src"],["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"]);
+    addTag("head", "script", ["src", "integrity", "crossorigin"], ["https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\n", "anonymous"]);
+    addTag("head", "script", ["src", "integrity", "crossorigin"],["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", "sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6", "anonymous"]);
+
+
+
     addTag("head", "link", ["src"],["themeBootstrap/assets/vendor/perfect-scrollbar/js/perfect-scrollbar.min.js"]);
     addTag("head", "link", ["src"],["themeBootstrap/assets/vendor/stickykit/js/stickykit.min.js"]);
 
 
-    //Firebase Stuff
-    addTag("html", "script", ["src"], ["/__/firebase/7.13.2/firebase-app.js"])
-    addTag("html", "script", ["src"], ["/__/firebase/7.13.2/firebase-analytics.js"])
-    addTag("html", "script", ["src"], ["/__/firebase/init.js"])
+    // //Firebase Stuff
+    // addTag("html", "script", ["src"], ["/__/firebase/7.13.2/firebase-app.js"])
+    // addTag("html", "script", ["src"], ["/__/firebase/7.13.2/firebase-analytics.js"])
+    // addTag("html", "script", ["src"], ["/__/firebase/init.js"])
 
+
+    $("body").append(
+        `<nav class="navbar navbar-expand-lg navbar-light bg-neutral-dark rounded p-3">
+            <div class="container">
+                <a href="index.html" class="navbar-brand align-middle text-center d-40 p-0" style="width: 88px!important;" href="#">
+                    <img src="img/navBarLogo.png" class="h-100">
+                </a>
+                <button class="navbar-toggler is-active collapsed hamburger hamburger--elastic border-0" type="button" data-toggle="collapse" data-target="#nav-primary" aria-controls="nav-primary" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+                <div class="collapse navbar-collapse" id="nav-primary">
+                    <ul class="navbar-nav w-100 mt-3">
+                        <li class="nav-item">
+                            <a class="nav-link py-0" href="">Tasks</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link py-0" href="#">Calendar</a>
+                        </li>
+                        <li id='settings' class="nav-item ml-auto">
+                            <a class="nav-link py-0" href="settings.html"> <i class="fas fa-cog"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>`
+    );
 
 
 
